a8e604645aae5c6707b8a029beea5af5d7448f4b
ester <estersilvade@gmail.com>
2021-12-17 19:04:47 -0300

    att

--- src/routes/prof_route.js
+++ src/routes/prof_route.js
@@ -11,23 +11,27 @@ const profController = require('../controllers/prof_contro')
 
 router.get('/prof', (req, res) => {
   res.status(200).json({
-    message:" Helpe Visinhos API iremos conectar pessoas prestadores de serviços  e solicitantes  da mesma região."
+    message:"Helpe Vizinhos API para conectar pessoas prestadores de serviços  e solicitantes  da mesma região."
    })
 
 })
 
 
-//@rout get '//criar' apresentação
+//@rout get '//criar' atividade
 //@dec Rota de teste com apresentação com
-//@acc public
+//@acc privada 
 router.post('/criar', profController.criar)
 
+
+//@rout get '/excluir/:id' Excuir atividade 
+//@dec Rota para excluir
+//@acc privada 
 router.delete('/excluir/:id', profController.excluir)
 
 
 
 //@rout get '/lista' apresentação
-//@dec Rota de teste com apresentação com
+//@dec lista de profissao
 //@acc public
 router.get('/lista/:', profController.lista)
 
