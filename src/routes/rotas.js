58cfa27c48d01899cc7164579ce399f2d2ca1af5
ester <estersilvade@gmail.com>
2021-12-18 08:47:16 -0300

    tudo pronta bora

--- src/routes/rotas.js
+++ src/routes/rotas.js
@@ -9,7 +9,7 @@ const userController = require('../controllers/user')
 //@acc public
 
 router.get('/', (req, res) => res.status(200).json({
-  mensage: "Helpe Visinhos "
+  menssagen:: "Helpe Visinhos "
 })) 
 
 //@ route Get /All
